65b96ee77034854a367dcdd12a47bed4f4bea7af
Czimpsky <maciek.grb@gmail.com>
2024-04-19 19:29:15 +0200

    mobile menu

--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
 import { style } from "./css/index.scss"
 
+Vue.createApp({
+    data(){
+        return{
+            menuOpen: false,
+        }
+    },
 
-// Vue.createApp({
-//     data(){
-//         return{
+    computed:{
+        navStyles(){
+            return{
+                height: this.menuOpen ? '100vh' : '100px',
+                position: this.menuOpen ? 'fixed' : 'absolute',
+                transition: 'height 0.3s ease-in-out',
+            }
+        },
 
-//         }
-//     }
-// }).mount("#app")
+        menuExtend(){
+            return{
+                opacity: this.menuOpen ? '1' : '0',
+                transition: 'opacity 0.4s ease-in-out',
+                transitionDelay: this.menuOpen ? '0s' : '0.4s'
+            }
+        }
+    },
 
+    methods: {
+        toggleMenuIcon(){
+            this.menuOpen = !this.menuOpen;
+        }
+    }
+}).mount("#app-nav")
